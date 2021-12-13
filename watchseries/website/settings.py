8d6d4a530aac20c98f1da35988dd748a96868149
Luziza <ribeiroluiza.jlle@gmail.com>
2021-12-13 14:46:11 -0300

    login

--- watchseries/website/settings.py
+++ watchseries/website/settings.py
@@ -126,5 +126,4 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = "{}/media".format(BASE_DIR)
+
